d6f32866ed34fc95dc367b9734945d393d119e6e
sushi3085 <itanki3085@gmail.com>
2022-07-08 12:44:57 +0800

    commit #1

---
+++ app/src/main/java/ncue/geo/avoidingcomingwater/MainActivity.kt
@@ -0,0 +1,11 @@
+package ncue.geo.avoidingcomingwater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
