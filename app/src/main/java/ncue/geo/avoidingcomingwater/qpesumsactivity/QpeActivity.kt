c5f4526c2b0f10657a9f637620dcbb0ccd184949
sushi3085 <itanki3085@gmail.com>
2022-08-14 18:36:04 +0800

    完成QPESUMS定位功能

--- app/src/main/java/ncue/geo/avoidingcomingwater/qpesumsactivity/QpeActivity.kt
+++ app/src/main/java/ncue/geo/avoidingcomingwater/qpesumsactivity/QpeActivity.kt
@@ -1,25 +1,32 @@
 package ncue.geo.avoidingcomingwater.qpesumsactivity
 
+import android.Manifest
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.Gravity
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import ncue.geo.avoidingcomingwater.ConditionController
 import ncue.geo.avoidingcomingwater.R
 import ncue.geo.avoidingcomingwater.crawlapi.HttpClient
 import org.json.JSONArray
-import java.math.BigDecimal
-import java.math.RoundingMode
 import kotlin.concurrent.thread
-import kotlin.math.roundToInt
 
 class QpeActivity : AppCompatActivity() {
+    private val locationListener = LocationListener { println("handler print: $it") }
+    private var location: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qpe)
@@ -111,19 +118,19 @@ class QpeActivity : AppCompatActivity() {
                 .show()
             return
         }
-        // get position choosen
-        val choosenTownName = findViewById<Spinner>(R.id.qpeZoneSpinner).selectedItem.toString()
-        val choosenPositionName =
-            findViewById<Spinner>(R.id.qpePositionSpinner).selectedItem.toString()
         // get response from api
         thread {
-            val response = HttpClient().get("https://470f-118-163-203-105.jp.ngrok.io/api/qpe/$choosenTownName/$choosenPositionName")
+            // get position choosen
+            val choosenTownName = findViewById<Spinner>(R.id.qpeZoneSpinner).selectedItem.toString()
+            val choosenPositionName = findViewById<Spinner>(R.id.qpePositionSpinner).selectedItem.toString()
+            val response = HttpClient().get("https://4b67-118-163-203-105.jp.ngrok.io/api/qpe/$choosenTownName/$choosenPositionName")
             // extract data
             val jsonArr = JSONArray(response)
             Log.wtf("qpe fetching data", response)
             // refresh UI
             runOnUiThread {
-                val rain = jsonArr[0].toString().toBigDecimal().setScale(1, RoundingMode.CEILING).toFloat()
+                val rain = jsonArr[0].toString().toFloat()
+                Log.wtf("print rain value", rain.toString())
                 findViewById<TextView>(R.id.qpeRainValueTV).text = "${jsonArr[0]}mm"
                 val rainLevel = if (rain >= 40)
                     "大雨或以上"
@@ -132,7 +139,7 @@ class QpeActivity : AppCompatActivity() {
                 else if (rain >= 10)
                     "小雨"
                 else if (rain >= 0)
-                    "晴/無雨"
+                    "晴/小雨"
                 else
                     "---"
                 findViewById<TextView>(R.id.qpeRainLevelValueTV).text = rain.toString()
@@ -149,11 +156,105 @@ class QpeActivity : AppCompatActivity() {
     }
 
     fun qpeLocateButtonOnClick(view: View) {
-        // locate
-        // set apinner
-        // call qpeLookup
-        qpeLookupButtonOnClick(view)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+        }
+        ConditionController.IS_POSITIONING = isGPSEnabled(baseContext)
+        if (!isGPSEnabled(baseContext)) {
+            alertPositioning()
+            return
+        }
+        // fetch location
+        fetchLocal()
+        // fetch nearest location
+        thread {
+            runOnUiThread { Toast.makeText(this, "請稍待定位抓取", Toast.LENGTH_LONG).show() }
+            Thread.sleep(5000)
+            val countySpinner = findViewById<Spinner>(R.id.qpeCountySpinner)
+            val townSpinner = findViewById<Spinner>(R.id.qpeZoneSpinner)
+            val positionSpinner = findViewById<Spinner>(R.id.qpePositionSpinner)
+            val jsonArr = JSONArray(HttpClient().get("https://4b67-118-163-203-105.jp.ngrok.io/api/nearestPosition/${location!!.longitude}/${location!!.latitude}"))
+
+            // set spinner
+            val countyListID = when (jsonArr.getString(0)) {// countyName
+                "新北市" -> 1
+                "桃園市" -> 2
+                "宜蘭縣" -> 3
+                "新竹縣" -> 4
+                "南投縣" -> 5
+                "台中市" -> 6
+                "高雄市" -> 7
+                "台東縣" -> 8
+                else -> 0
+            }
+            runOnUiThread {
+                countySpinner.setSelection(countyListID)
+
+                var townID = -1
+                val townName = jsonArr.getString(1)
+                for (i in 0 until townSpinner.adapter.count)
+                    if (townName == townSpinner.adapter.getItem(i))
+                        townID = i
+                townSpinner.setSelection(townID)
+
+                val positionName = jsonArr.getString(2)
+                var positionID = -1
+                for (i in 0 until positionSpinner.adapter.count)
+                    if (positionName == positionSpinner.adapter.getItem(i))
+                        positionID = i
+                positionSpinner.setSelection(positionID)
+                // call qpeLookup
+                thread {
+                    Thread.sleep(500)//等一下，不然會find到null的View
+                    runOnUiThread { qpeLookupButtonOnClick(view) }
+                }
+            }
+        }
+
         return
+    }
+
+    private fun alertPositioning() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setIcon(R.drawable.popout_warning_icon)    // 取得小綠人圖示
+        dialog.setTitle("定位已關閉")
+        dialog.setMessage("您似乎沒有開啟手機的衛星定位功能\n請確認已開啟定位功能")
+        dialog.setNegativeButton("開啟設定") { _, _ ->
+//                Toast.makeText(this, "繼續使用", Toast.LENGTH_LONG).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            baseContext.startActivity(intent)
+        }
+
+        dialog.setPositiveButton("知道了") { _, _ ->
+//                Toast.makeText(this, "結束程式", Toast.LENGTH_LONG).show()
+        }
+        dialog.show()
+    }
+
+    private fun fetchLocal(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                /* 那就 */
+                return
+            }
+        }
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,999999F,locationListener)
+
+        /** 知道位置之後，取得位置資訊 **/
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun isGPSEnabled(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 }
 
